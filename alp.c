901e03bad09cb7b7cdcc9f8e8bdf13870c8cfd15
kathirvelkgm <31503336+kathirvelkgm@users.noreply.github.com>
2018-01-19 16:23:40 +0530

    Rename alpha.c to alp.c

---
+++ alp.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    char k;
+    printf("Enter any character: ");
+    scanf("%c", &k);
+   if((k >= 'a' && ch <= 'z') || (k >= 'A' && ck <= 'Z'))
+    {
+        printf("Character is an ALPHABET");
+    }
+    else
+    {
+        printf("Character is not ALPHABET");
+    }
+
+    return 0;
+}
+
