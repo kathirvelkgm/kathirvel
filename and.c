b9fa27b06f016b120b7f708b3478062fbb3e89d1
kathirvelkgm <31503336+kathirvelkgm@users.noreply.github.com>
2018-01-23 11:17:29 +0000

    Create and.c

---
+++ and.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() 
+{
+	int a,d,n,sum,v,k,j,i=0,m,t;
+	scanf("%d%d%d",&a,&n,&d);
+	if(i<n)
+	{
+		i=i+a;
+		sum=a+i;
+		sum=v;
+		k=v+d;
+		k=m;
+		j=m+d;
+		t=sum+k+j;
+		printf("the value is %d",t);
+	}
+	return 0;
+}
