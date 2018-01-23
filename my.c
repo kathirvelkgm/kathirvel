d39290a0c6119813ce26894c54fcb1e605535f93
kathirvelkgm <31503336+kathirvelkgm@users.noreply.github.com>
2018-01-23 11:31:16 +0000

    Create my.c

---
+++ my.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() 
+{
+	int a,d,n,v,k,j,i=0,m,t,o;
+	scanf("%d%d%d",&n,&a,&d);
+		o=i+a;
+		v=o;
+		k=v+d;
+		m=k;
+		j=m+d;
+		t=o+k+j;
+		printf("the value is %d",t);
+	return 0;
+}
