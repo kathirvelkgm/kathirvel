a55aa9f406675c3c0b8f0bbfd569cd247dedc3b3
kathirvelkgm <31503336+kathirvelkgm@users.noreply.github.com>
2018-02-02 15:14:57 +0530

    Create countspace.c

---
+++ countspace.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int a,sum=0,i=0;
+char b[20]={"hello world u u u"};
+while(b[i]!='\0')
+{
+	if(b[i]==' ')
+	{
+		sum++;
+	}
+
+	i++;
+}
+printf("no of space %d",sum);
+return 0;
+
+}
