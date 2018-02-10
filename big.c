4b33431d8610eb343ce71bbfd922f9bb7f72d403
kathirvelkgm <31503336+kathirvelkgm@users.noreply.github.com>
2018-02-10 12:20:28 +0530

    Create big.c

---
+++ big.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+{     
+	int l,i,r,h,s,o,a,b,c,d,e,f,g,z=1;
+	scanf("%d%d%d%d%d",&l,&i,&r,&h,&s);
+	if((l==z)&&(i==z)&&(r==z)&&(h==z)&&(s==z)(o==z))
+	{
+		printf("congrats");
+	}
+            	elseif((l!=z)&&(i!=z)&&(r!=z)&&(h!=z)&&(s!=z)(o!=z))
+            	{
+            		printf("pay 3250");
+            	}
+            	elseif((l==z)&&(i==z)&&(r==z)&&(h==z)&&(s==z)(o!=z))
+            	{
+            		printf("pay 350");
+            	}
+            	elseif((l==z)&&(i==z)&&(r==z)&&(h==z)&&(s!=z)(o!=z))
+            	{
+            		printf("pay 650");
+            	}
+            	elseif((l==z)&&(i==z)&&(r==z)&&(h!=z)&&(s!=z)(o!=z))
+            	{
+            		printf("pay 1050");
+            	}
+            	elseif((l==z)&&(i==z)&&(r!=z)&&(h!=z)&&(s!=z)(o!=z))
+                        {
+	                        printf("pay 1750");
+                        }
+                        elseif((l==z)&&(i!=z)&&(r!=z)&&(h!=z)&&(s!=z)(o!=z))
+                        {
+                        	printf("pay 2250");
+                        }
+                        elseif((l!=z)&&(i==z)&&(r==z)&&(h==z)&&(s==z)(o==z))
+                        {
+                        	printf("pay 1000");
+                        	
+                        }
+                        elseif((l!=z)&&(i!=z)&&(r==z)&&(h==z)&&(s==z)(o==z))
+                        {
+                        	printf("pay 1500");
+                        }
+                        
+                        }
+
+                      
+
+            	
+            
+        
