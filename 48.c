26600fbea7ba8bd708fb6426225a36184918a2a4
kathirvelkgm <31503336+kathirvelkgm@users.noreply.github.com>
2018-02-22 16:25:28 +0530

    Create 48.c

---
+++ 48.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int n,sum=0,i;
+	int a[10];
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+scanf("%d",&a[i]);
+}
+	{
+		for(i=0;i<n;i++)
+	sum=sum+a[i];	
+	
+	}
+	printf("%d",sum/n);
+	return 0;
+}
+	
